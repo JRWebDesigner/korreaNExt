4bf113d49ea910e29b831cafb916709085a2d056
JRwebdesing <jowebdesigc@gmail.com>
2025-05-07 11:02:54 -0400

    init next

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="relative h-[130dvh] bg-black">
+	      <video src="ejemplo.mp4" className="relative z-0 w-full h-full object-cover" autoplay muted>
+	      </video>
+      </section>
+      <section className="bg-[url(/svg.svg)] bg-cover h-[90dvh] w-full flex justify-center items-center flex-col gap-10 p-6">
+          <h2 className="mx-22 font-bold text-xl md:text-6xl text-center text-green-400">
+            Creative Event Agency &
+Event Production Company
+          </h2>
+          <p className="mx-22 text-center font-medium text-2xl text-blue-300">
+          We blend creative agency thinking with production company doing to deliver extraordinary events and everlasting experiences
+<br />
+Enabling you to create an experience that matters for the people that matter to you
+          </p>
+      </section>
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+// components/Header.js
+'use client';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="fixed w-full z-50">
+      <header
+        className={`${
+          scrolled ? 'bg-gray-900/30 backdrop-blur-sm shadow-lg' : 'bg-transparent'
+        } text-white px-4 md:px-10 py-4 w-full flex flex-col md:flex-row justify-between items-center m-auto rounded-none duration-500 transition-all ease-in-out`}
+      >
+        <div className="flex justify-between items-center w-full md:w-auto">
+          <Link href="/">
+            <img src="/logo.png" alt="Logo" className='h-16'/>
+          </Link>
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="md:hidden text-white text-3xl focus:outline-none"
+          >
+            &#9776;
+          </button>
+        </div>
+        <nav className={`${menuOpen ? 'flex' : 'hidden'} md:flex md:w-1/2 w-full mt-4 md:mt-0`}>
+          <ul className="flex flex-col md:flex-row justify-around items-center text-3xl md:text-xl font-semibold w-full">
+            <li className="transition-colors duration-100 hover:text-green-400">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="transition-colors duration-100 hover:text-green-400">
+              <Link href="/about">About us</Link>
+            </li>
+            <li className="transition-colors duration-100 hover:text-green-400">
+              <Link href="/contact">What we do</Link>
+            </li>
+            <li className="transition-colors duration-100 hover:text-green-400">
+              <Link href="/about">Our work</Link>
+            </li>
+            <li className="transition-colors duration-100 hover:text-green-400">
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
