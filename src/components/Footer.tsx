c6ffdc39c69ff26e05d4b67f932cf2a52c3fd33f
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-05-17 10:04:13 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,24 @@ export default function Footer(){
         <footer className="bg-black text-white py-10 px-6 md:px-20">
       <div className="flex flex-col md:flex-row justify-between items-center mb-10">
         <div className="flex items-center gap-4 mb-6 md:mb-0">
-          <Image src="/img/logo-ec.png" alt="Event Concept logo" width={60} height={60} />
+          <Image src="/logo.png" alt=" logo" width={60} height={60} />
           <div className="text-sm">
-            <p className="text-green-400 font-medium">+44 (0) 20 7740 3988</p>
-            <p className="text-green-400 font-medium">info@eventconcept.com</p>
+            <p className="text-green-400 font-medium">+44 7939 697888</p>
+            <p className="text-green-400 font-medium">info@korreastudio.com</p>
           </div>
         </div>
         <div className="text-sm text-right">
-          <p>London | New York | Global</p>
+          <p>London, United Kingdom</p>
         </div>
       </div>
 
       <div className="flex flex-wrap justify-center gap-6 border-t border-gray-700 pt-6">
-        {["EXPERTISE", "OUR WORK", "INSIGHTS", "ABOUT US", "VENUES", "SUSTAINABILITY"].map((item) => (
-          <Link key={item} href="#" className="text-white font-semibold text-sm hover:text-green-400">
-            {item}
-          </Link>
-        ))}
+        <Link href="/" className="text-white font-semibold text-sm hover:text-green-400">>Home</Link>
+              <Link href="/about-us" className="text-white font-semibold text-sm hover:text-green-400">>About us</Link>
+     
+              <Link href="/what-we" className="text-white font-semibold text-sm hover:text-green-400">>What we do</Link>
+              <Link href="/our-work" className="text-white font-semibold text-sm hover:text-green-400">>Our work</Link>
+              <Link href="/contact" className="text-white font-semibold text-sm hover:text-green-400">>Contact</Link>
         <div className="flex items-center gap-4 ml-4 text-green-400">
           <Link href="https://www.instagram.com/korreastudio?igsh=MW1kOTQza25vMW9ocw=="><FaInstagram size={20} /></Link>
         </div>
