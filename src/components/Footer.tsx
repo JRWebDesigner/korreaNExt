66c8ea4c54e8103c36101b77030fb2176665f977
Josue <jowebdesigc@gmail.com>
2025-09-02 18:42:46 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,25 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Footer(){
   return(
-        <footer className="relative z-50 bg-black text-white py-10 px-6 md:px-20 flex flex-col items-center justify-center">
-          <Image src="/logo.png" alt=" logo" width={140} height={140} />
-          <div className="text-sm">
-            <p className="text-green-400 font-medium">+44 7939 697888</p>
-            <p className="text-green-400 font-medium">info@korreastudio.co.uk</p>
-          </div>
-        <div className="text-sm text-right">
-          <p>London, United Kingdom</p>
-        </div>
-
-      <div className="flex flex-wrap justify-center gap-6 border-t border-gray-700 pt-6">
-        <Link href="/" className="text-white font-semibold text-sm hover:text-green-400">Home</Link>
-              <Link href="/about-us" className="text-white font-semibold text-sm hover:text-green-400">About us</Link>
-     
-              <Link href="/what-we" className="text-white font-semibold text-sm hover:text-green-400">What we do</Link>
-              <Link href="/our-work" className="text-white font-semibold text-sm hover:text-green-400">Our work</Link>
-              <Link href="/contact" className="text-white font-semibold text-sm hover:text-green-400">Contact</Link>
-        <div className="flex items-center gap-4 ml-4 text-green-400">
+        <footer className="relative z-50 bg-black text-white py-16 px-6 md:px-20 flex flex-col items-center justify-center w-full">
+          <img src="/logo.png" alt="logo" className="h-[100px]" />
+          <div className="text-lg flex items-center justify-center gap-7 w-full text-white">
+            <p className="font-medium">+44 7939 697888</p>
+            <p className="font-medium">info@korreastudio.co.uk</p>
+            <p className="font-medium">London, United Kingdom</p>
           <Link href="https://www.instagram.com/korreastudio?igsh=MW1kOTQza25vMW9ocw=="><FaInstagram size={20} /></Link>
+         </div>
+     
+      <div className="w-full flex flex-wrap justify-center gap-6  pt-6 text-xl">
+        <Link href="/" className="text-white font-semibold  hover:text-green-400">Home</Link>
+              <Link href="/about-us" className="text-white font-semibold hover:text-green-400">About us</Link>
+     
+              <Link href="/what-we" className="text-white font-semibold hover:text-green-400">What we do</Link>
+              <Link href="/our-work" className="text-white font-semibold hover:text-green-400">Our work</Link>
+              <Link href="/contact" className="text-white font-semibold hover:text-green-400">Contact</Link>
+        <div className="flex items-center gap-4 ml-4 text-green-400">
+          
         </div>
       </div>
     </footer>
