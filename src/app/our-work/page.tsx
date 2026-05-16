6dee395db5eccc293065029b472e57c24ddc9a05
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-05-16 17:04:13 -0400

    Adjust section height and alignment in Ourwork component

--- src/app/our-work/page.tsx
+++ src/app/our-work/page.tsx
@@ -17,7 +17,7 @@ const eventData = [
 export default function Ourwork(){
   return(
     <>
-      <section className="relative h-[40dvh] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-start">
+      <section className="relative h-[320px] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-center">
         <div className="container mx-auto">
             <h1 className="text-4xl md:text-6xl font-bold text-red-600 mb-4 text-center">
               Work
