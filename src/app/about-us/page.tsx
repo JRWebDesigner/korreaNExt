3302399406535562bee520b7a30ed41c8104e471
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-05-27 15:51:00 -0400

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,7 +7,7 @@ export default function Aboutus() {
             Studio
           </h1>
           <p className="container text-xl text-white text-center mt-0">
-            Korreastudio is a creative studio specializing in set design and spatial building for visual storytelling. We create intentional environments that shape mood, narrative, and image.
+            Korrea Studio is a creative studio specializing in set design and spatial building for visual storytelling. We create intentional environments that shape mood, narrative, and image.
           </p>
         </div>
       </section>
