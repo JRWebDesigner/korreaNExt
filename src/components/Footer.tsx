489e5136459afc4bcbb39d69c8669980cc8912e7
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-05-27 15:33:23 -0400

    Refactor Footer component for improved structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,51 @@
-"use client"
+"use client";
 import { FaInstagram } from "react-icons/fa";
 import Link from "next/link";
-export default function Footer(){
-  return(
-        <footer className="relative z-50 bg-[url(/fondoB.jpg)] text-white py-16 px-6 md:px-20 flex flex-col items-center justify-center w-full gap-5">
-          <img src="/logo_w.png" alt="logo" className="absolute opacity-10 bottom-6 w-[80%] z-10" />
-          <Link href='/'>
-            <img src="/logo_w.png" alt="logo" className="h-[50px]" />
-          </Link>
-          <div className="relative z-20 text-lg flex items-center justify-center gap-7 w-full text-white opacity-70">
-           
-            <p className="font-medium">info@korreastudio.co.uk</p>
-            
-          
-         </div>
-          <Link href="https://www.instagram.com/korreastudio?igsh=MW1kOTQza25vMW9ocw=="><FaInstagram size={20} /></Link>
-     <p className="relative z-20 font-medium opacity-70">London, United Kingdom</p>
-      <div className="relative z-20 w-full flex flex-wrap justify-center gap-6 mt-10 text-xl">
-        <Link href="/our-work" className="text-white font-semibold hover:text-red-600">Work</Link>
-        <Link href="/about-us" className="text-white font-semibold hover:text-red-600">Studio</Link>
-        <Link href="/services" className="text-white font-semibold hover:text-red-600">Services</Link>
-        <Link href="/contact" className="text-white font-semibold hover:text-red-600">Contact</Link>
-      <div className="flex items-center gap-4 ml-4 text-green-400">
+
+export default function Footer() {
+  return (
+    <footer className="relative z-50 bg-[url(/fondoB.jpg)] text-white py-16 px-6 md:px-20 flex flex-col items-center justify-center w-full gap-5">
+      {/* Imagen de fondo centrada (horizontal y opcionalmente vertical) */}
+      <img
+        src="/logo_w.png"
+        alt="logo background"
+        className="absolute opacity-10 w-[80%] z-10 left-1/2 -translate-x-1/2 bottom-6"
+        /* Si también quieres centrado vertical: reemplaza bottom-6 por top-1/2 -translate-y-1/2 */
+      />
+
+      {/* Logo principal */}
+      <Link href="/">
+        <img src="/logo_w.png" alt="logo" className="h-[50px]" />
+      </Link>
+
+      {/* Correo electrónico */}
+      <div className="relative z-20 text-lg flex items-center justify-center w-full text-white opacity-70">
+        <p className="font-medium">info@korreastudio.co.uk</p>
       </div>
+
+      {/* Instagram */}
+      <Link href="https://www.instagram.com/korreastudio?igsh=MW1kOTQza25vMW9ocw==">
+        <FaInstagram size={20} />
+      </Link>
+
+      {/* Ubicación */}
+      <p className="relative z-20 font-medium opacity-70">London, United Kingdom</p>
+
+      {/* Enlaces de navegación */}
+      <div className="relative z-20 w-full flex flex-wrap justify-center gap-6 mt-10 text-xl">
+        <Link href="/our-work" className="text-white font-semibold hover:text-red-600">
+          Work
+        </Link>
+        <Link href="/about-us" className="text-white font-semibold hover:text-red-600">
+          Studio
+        </Link>
+        <Link href="/services" className="text-white font-semibold hover:text-red-600">
+          Services
+        </Link>
+        <Link href="/contact" className="text-white font-semibold hover:text-red-600">
+          Contact
+        </Link>
       </div>
     </footer>
-  )
+  );
 }
