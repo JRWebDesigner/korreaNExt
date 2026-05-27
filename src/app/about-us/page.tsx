333297f476c0dceca2c49ebef10d717dbd593895
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-05-27 15:36:44 -0400

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,12 +1,12 @@
 export default function Aboutus() {
   return (
     <>
-      <section className="relative h-[180px] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-start">
+      <section className="relative h-[160px] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-start -mt-5">
         <div className="container mx-auto">
             <h1 className="text-4xl md:text-5xl font-bold text-red-600 mb-4 text-center">
               Studio
             </h1>
-            <p className="text-xl text-white text-center">
+            <p className="container text-xl text-white text-center">
               Korreastudio is a creative studio specializing in set design and spatial building for visual storytelling. We create intentional environments that shape mood, narrative, and image.
             </p>
         </div>
