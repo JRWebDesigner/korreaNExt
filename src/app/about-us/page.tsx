2b7b8a4247ee047a57f3dc746ac5ef00385e1d27
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-05-27 15:38:16 -0400

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,7 +1,7 @@
 export default function Aboutus() {
   return (
     <>
-      <section className="relative h-[160px] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-start -mt-5">
+      <section className="relative h-[160px] bg-[url(/fondoB.jpg)] bg-cover flex justify-center items-start">
         <div className="container mx-auto">
             <h1 className="text-4xl md:text-5xl font-bold text-red-600 mb-4 text-center">
               Studio
