59226d45f8bbb084e2d6c1fa63046720d9a1495d
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-08-27 17:55:13 -0400

    ancho page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,14 +35,14 @@ export default function Home() {
 
   return (
     <>
-      <section className="relative grid grid-cols-[0.5fr_2fr_0.5fr] h-[70dvh] md:h-[80dvh] bg-black max-w-[1400px] mx-auto">
+      <section className="relative grid grid-cols-[0.5fr_2fr_0.5fr] h-[70dvh] md:h-[80dvh] bg-black max-w-[1600px] mx-auto">
         <div className="bg-gray-200 h-full" /> 
         <video src="ejemplo.mp4" className="relative z-0 w-full h-full object-cover" autoPlay loop playsInline muted>
         </video>
          <div className="bg-gray-200 h-full" /> 
       </section>
 
-      <section className="flex flex-col items-center mb-16 max-w-[1400px] mx-auto">
+      <section className="flex flex-col items-center mb-16 max-w-[1600px] mx-auto">
         <div className="w-full bg-black py-14 md:py-20 px-6 md:px-20">
           <div className="max-w-3xl mx-auto text-center">
             <p className="italic text-base md:text-3xl leading-relaxed text-gray-200">
@@ -65,7 +65,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full max-w-[1400px] mx-auto">
+      <section className="w-full max-w-[1600px] mx-auto">
         <div className="grid grid-cols-12">
           {/* Franja superior gris claro */}
           <div className="col-span-12 bg-white h-24 md:h-[400px]" />
