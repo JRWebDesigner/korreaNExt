a22a7d9f78bd4247768e4edfde28701e77b8de46
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-08-27 18:37:01 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,18 +65,16 @@ export default function Home() {
         </div>
       </section>
       <section className="w-full max-w-[1600px] mx-auto">
-        <div className="grid grid-cols-12">
-          {/* Franja superior con textura tipo papel/concreto */}
-          <div
-            className="col-span-12 h-[35dvh] md:h-[35.5dvh] bg-[#efece4] bg-[url('/texture-paper.jpg')] bg-cover bg-center"
-          />
+        <div className="grid grid-cols-12 grid-rows-2">
+          {/* Naranja: ocupa TODO el alto, ambas franjas */}
+          <div className="col-span-2 row-span-2 bg-orange-600 h-[71dvh]" />
       
-          {/* Fila 2: naranja / negro / imagen con textura */}
-          <div className="col-span-2 bg-orange-600 h-[35dvh] md:h-[35.5dvh]" />
-          <div className="col-span-8 bg-black h-[35dvh] md:h-[35.5dvh]" />
-          <div
-            className="col-span-2 h-[35dvh] md:h-[35.5dvh] bg-[#dedad2] bg-[url('/texture-paper.jpg')] bg-cover bg-center"
-          />
+          {/* Franja superior con textura (cols 3-12) */}
+          <div className="col-span-10 h-[35.5dvh] bg-[#efece4] bg-[url('/texture-paper.jpg')] bg-cover bg-center" />
+      
+          {/* Fila 2: negro + textura (cols 3-12) */}
+          <div className="col-span-8 h-[35.5dvh] bg-black" />
+          <div className="col-span-2 h-[35.5dvh] bg-[#dedad2] bg-[url('/texture-paper.jpg')] bg-cover bg-center" />
         </div>
       </section>
       <section className="w-full max-w-[1600px] mx-auto">
